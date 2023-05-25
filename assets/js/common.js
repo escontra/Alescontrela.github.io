2ce8aa4f9aef00d994e75a8a6ba3424fd2aa0e35
alescontrela <alejandroescontrela@gmail.com>
2023-05-25 13:09:05 -0700

    Add hyperlinks to project sections

--- assets/js/common.js
+++ assets/js/common.js
@@ -7,3 +7,16 @@ $(document).ready(function() {
     });
     $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
 });
+
+const headings = document.querySelectorAll('h2[id],h3[id]'); // 1
+const linkContent = '🔗'; // 2
+for (const heading of headings) { // 3
+    const linkIcon = document.createElement('a'); // 4
+    linkIcon.setAttribute('href', `#${heading.id}`); // 5
+    linkIcon.innerHTML = linkContent; // 6
+    const linkSpace = document.createElement('p');
+    linkSpace.style.display = 'inline';
+    linkSpace.innerText = ' ';
+    heading.appendChild(linkSpace);
+    heading.appendChild(linkIcon); // 7
+}
